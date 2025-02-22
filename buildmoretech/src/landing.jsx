377a972b40c00c105b37c4d983e2b47c8b24a3ca
afiab <afiabidica@gmail.com>
2025-02-22 15:15:07 -0500

    made a landing page

---
+++ buildmoretech/src/landing.jsx
@@ -0,0 +1,14 @@
+function Landing() {
+    
+    return (
+        <div class="landing-container">
+            <h1>Build More Tech</h1>
+            <h2>Build More from Less</h2>
+            <a href="index.html">
+                <button>Get Started</button>
+            </a>
+        </div>
+    )
+}   
+
+export default Landing;
--- buildmoretech/src/main.jsx
+++ buildmoretech/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Landing from './landing.jsx'
+import './landing.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <Landing />
     
   </StrictMode>,
 )
